350244f5dafc4ab42e603653fa92ddd043bb1393
EndriuK <endrit.kaskija@gmail.com>
2024-07-04 17:38:18 +0200

    [UPD] stampo in console numeroPC e somma

--- es-2/js/script.js
+++ es-2/js/script.js
@@ -3,3 +3,13 @@ let pariDispari = prompt('Scegli "pari o dispari"');
 
 // CHIEDO ALL'UTENTE DI SCEGLIERE UN NUMERO DA 1 A 5
 let numeroUser = parseInt(prompt("Scegli un numero da 1 a 5"));
+
+// GENERO UN NUMERO CASUALE PER IL COMPUTER
+let numbAI = numeroComputer();
+
+// CALCOLO LA SOMMA DEI NUMERI
+let somma = numeroUser + numbAI;
+
+// STAMPO IL NUMERO DEL COMPUTER E LA SOMMA IN CONSOLE
+console.log("Numero PC: " + numbAI);
+console.log("Somma: " + somma);
