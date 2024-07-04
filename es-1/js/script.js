159cec7e89f888c476f8bd20e384e8a4dd0955a9
EndriuK <endrit.kaskija@gmail.com>
2024-07-04 16:49:16 +0200

    [UPD] funzione

--- es-1/js/script.js
+++ es-1/js/script.js
@@ -1,2 +1,10 @@
 // CHIEDO ALL'UTENTE DI INSERIRE UNA PAROLA
 let parola = prompt("Inserisci parola");
+
+// VARIABILE PER PAROLA INVERTITA
+let reverse;
+
+// FUNZIONE PER INVERTIRE LA PAROLA
+function reverseString(parola) {
+  reverse = parola.split("").reverse().join("");
+}
