fa14b1f042cc9fab23cbeaca239bc356afb1beef
EndriuK <endrit.kaskija@gmail.com>
2024-07-04 17:32:42 +0200

    [UPD] chiedo all'utente pari o dispari + numeri

---
+++ es-2/js/script.js
@@ -0,0 +1,5 @@
+// CHIEDO ALL'UTENTE DI SCEGLIERE "PARI" O "DISPARI"
+let pariDispari = prompt('Scegli "pari o dispari"');
+
+// CHIEDO ALL'UTENTE DI SCEGLIERE UN NUMERO DA 1 A 5
+let numeroUser = parseInt(prompt("Scegli un numero da 1 a 5"));
